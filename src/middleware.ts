9cc4e986e1ee21aabe26a41bd6f494db825586b2
Sandeepkumar9971 <Sk13270906@gmail.com>
2024-06-22 16:00:59 +0530

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -30,16 +30,16 @@ export async function middleware(request: NextRequest) {
       url.pathname.startsWith("/dashboard"))
   ) {
    
-    // if (token && (url.pathname.startsWith('/login_signup') || url.pathname.startsWith('/dashboard'))) {
-    //     // If user is already on their role-based dashboard, do not redirect
-    //     if (url.pathname !== `/dashboard/${role}`) {
-    //         return NextResponse.redirect(new URL(`/dashboard/${role}`, request.url));
-    //     }
-    // }
+    if (token && (url.pathname.startsWith('/login_signup') || url.pathname.startsWith('/dashboard'))) {
+        // If user is already on their role-based dashboard, do not redirect
+        if (url.pathname !== `/dashboard/${role}`) {
+            return NextResponse.redirect(new URL(`/dashboard/${role}`, request.url));
+        }
+    }
 
-    // if (!token && url.pathname.startsWith('/dashboard')) {
-    //     return NextResponse.redirect(new URL('/login_signup', request.url));
-    // }
+    if (!token && url.pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/login_signup', request.url));
+    }
 
 
     return NextResponse.next();
